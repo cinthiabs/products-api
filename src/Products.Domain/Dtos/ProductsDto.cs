beb419276f40a24f5f2df3db22d6b3cfea6c9935
Cinthia Barbosa da Silva <cinthiabarbosa8d@outlook.com>
2025-05-29 07:58:40 -0300

    add dtos

---
+++ src/Products.Domain/Dtos/ProductsDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Products.Domain.Dtos;
+
+public class ProductsDto
+{
+    public int ProductId { get; init; }
+    public string Name { get; init; } = default!;
+    public string Description { get; init; } = default!;
+    public decimal Price { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public IEnumerable<ProductItemDto> Items { get; init; } = default!;
+}
+
+public class ProductItemDto
+{
+    public int ItemId { get; init; }
+    public int ProductId { get; init; }
+    public int Quantity { get; init; }
+    public string BatchNumber { get; init; } = default!;
+}
