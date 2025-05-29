2d8d525f83492a42b8a2c936379817e8015bf57a
Cinthia Barbosa da Silva <cinthiabarbosa8d@outlook.com>
2025-05-29 08:02:41 -0300

    add properties

--- src/Products.Application/Products/Commands/CreateProducts/CreateProductsCommand.cs
+++ src/Products.Application/Products/Commands/CreateProducts/CreateProductsCommand.cs
@@ -1,5 +1,13 @@
-﻿namespace Products.Application.Products.Commands.CreateProducts;
+﻿using Ardalis.Result;
+using MediatR;
+using Products.Domain.Dtos;
 
-public class CreateProductsCommand
+namespace Products.Application.Products.Commands.CreateProducts;
+
+public class CreateProductsCommand : IRequest<Result>
 {
+    public string Name { get; init; } = default!;
+    public string Description { get; init; } = default!;
+    public decimal Price { get; init; }
+    public IEnumerable<ProductItemDto> Items { get; init; } = default!;
 }
