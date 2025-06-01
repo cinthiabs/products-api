e86db3de0ac75c9bd9f21f7ff34fba013feceaba
Cinthia Barbosa da Silva <cinthiabarbosa8d@outlook.com>
2025-05-31 21:38:48 -0300

    add repositoryBase

---
+++ src/Products.Infrastructure/Scripts/RepositoryBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace Products.Infrastructure.Scripts;
+
+public class RepositoryBase(IConfiguration configuration) : IDisposable
+{
+    private readonly IDbConnection _conn = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+
+    public IDbConnection Connection
+    {
+        get
+        {
+            if (_conn.State != ConnectionState.Open)
+            {
+                _conn.Open();
+            }
+
+            return _conn;
+        }
+    }
+
+
+    public void Dispose()
+    {
+        _conn?.Dispose();
+    }
+
+}
