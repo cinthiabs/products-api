f86adbff039ac7876807837fd02c98222b28990d
Cinthia Barbosa da Silva <cinthiabarbosa8d@outlook.com>
2025-06-02 14:23:17 -0300

    add extensions

--- src/Products.API/Program.cs
+++ src/Products.API/Program.cs
@@ -1,13 +1,27 @@
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Products.API.Extensions;
 using Products.Application.Extensions;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Host.AddSerilog();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMediatRApi();
+builder.Services.AddSwaggerUi();
+builder.Services.AddSerilog();
+
+builder.Services.AddApiVersioning(config =>
+{
+    config.DefaultApiVersion = new ApiVersion(1, 0);
+    config.AssumeDefaultVersionWhenUnspecified = true;
+    config.ReportApiVersions = true;
+    config.ApiVersionReader = new UrlSegmentApiVersionReader();
+});
 
 
 var app = builder.Build();
--- src/Products.Application/Products/Queries/GetProducts/GetProductsQueryHandler.cs
+++ src/Products.Application/Products/Queries/GetProducts/GetProductsQueryHandler.cs
@@ -4,10 +4,28 @@ using Microsoft.Extensions.Logging;
 
 namespace Products.Application.Products.Queries.GetProducts;
 
-public class GetProductsQueryHandler(ILogger<GetProductsQueryHandler> logger) : IRequestHandler<GetProductsQuery, Result<GetProductsQuery>>
+public class GetProductsQueryHandler(ILogger<GetProductsQueryHandler> logger) : IRequestHandler<GetProductsQuery, Result<GetProductsViewModel>>
 {
-    public Task<Result<GetProductsQuery>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+    public Task<Result<GetProductsViewModel>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var viewModel = new GetProductsViewModel()
+        {
+            ProductId = 1,
+            CreatedAt = DateTime.UtcNow,
+            Name = "Test",
+            Description = "Description",
+            Price = 30,
+            Items = new List<GetProductsItemModel>
+            {
+                new GetProductsItemModel
+                {
+                     ProductId = 1,
+                     BatchNumber = "AE781AED",
+                     Quantity = 1
+                }
+            }
+        };
+
+        return Task.FromResult(Result.Success(viewModel));
     }
 }
